6b2789afef1650763102a4a78e1dae9346200f6c
Alexander <elementoffchaos@gmail.com>
2019-10-08 14:31:11 +0700

    Updates

---
+++ precache-manifest.276e4e7af3dea96934eb04dc1b69dbce.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "080ec02cf2249392b56b83320ba88115",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "888e527f4e9cd78cf714",
+    "url": "/portfolio/static/css/main.39d2a699.chunk.css"
+  },
+  {
+    "revision": "d8f4a2564ca6133a78de",
+    "url": "/portfolio/static/js/2.21289823.chunk.js"
+  },
+  {
+    "revision": "888e527f4e9cd78cf714",
+    "url": "/portfolio/static/js/main.1c1ae092.chunk.js"
+  },
+  {
+    "revision": "ce083fbeab51ff3c6d59",
+    "url": "/portfolio/static/js/runtime-main.50c01cf9.js"
+  },
+  {
+    "revision": "7c9a05b7e3c93c4f6b6d133cd4e06dc7",
+    "url": "/portfolio/static/media/check_big.7c9a05b7.png"
+  }
+]);
