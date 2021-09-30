cc5b4fcc95b6f5115629743a6438b278e080c850
alexander <elementoffchaos@gmail.com>
2021-09-30 11:21:59 +0700

    github link added to footer social links

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,44 +1,49 @@
-import React from 'react';
+import React from "react";
 
 const socialList = [
-  {
-    name: 'facebook',
-    icon: 'fab fa-facebook-f',
-    link: 'https://web.facebook.com/alexander.pershin.125'
-  },
-  {
-    name: 'vk',
-    icon: 'fab fa-vk',
-    link: 'https://vk.com/id409604155'
-  },
-  {
-    name: 'telegram',
-    icon: 'fab fa-telegram-plane',
-    link: 'https://t.me/AlexanderPershin'
-  }
+    {
+        name: "facebook",
+        icon: "fab fa-facebook-f",
+        link: "https://web.facebook.com/alexander.pershin.125",
+    },
+    {
+        name: "vk",
+        icon: "fab fa-vk",
+        link: "https://vk.com/id409604155",
+    },
+    {
+        name: "telegram",
+        icon: "fab fa-telegram-plane",
+        link: "https://t.me/AlexanderPershin",
+    },
+    {
+        name: "github",
+        icon: "fab fa-github",
+        link: "https://github.com/AlexanderPershin",
+    },
 ];
 
 const Footer = () => {
-  const renderSocials = () => {
-    return socialList.map(item => (
-      <a
-        className='footer__social'
-        key={item.name}
-        href={item.link}
-        target='_blank'
-        rel='noopener noreferrer'
-      >
-        <i className={item.icon}></i>
-      </a>
-    ));
-  };
+    const renderSocials = () => {
+        return socialList.map((item) => (
+            <a
+                className="footer__social"
+                key={item.name}
+                href={item.link}
+                target="_blank"
+                rel="noopener noreferrer"
+            >
+                <i className={item.icon}></i>
+            </a>
+        ));
+    };
 
-  return (
-    <footer className='footer'>
-      <div className='footer__socials'>{renderSocials()}</div>
-      &copy; All rights reserved {new Date().getFullYear()}
-    </footer>
-  );
+    return (
+        <footer className="footer">
+            <div className="footer__socials">{renderSocials()}</div>
+            &copy; All rights reserved {new Date().getFullYear()}
+        </footer>
+    );
 };
 
 export default Footer;
